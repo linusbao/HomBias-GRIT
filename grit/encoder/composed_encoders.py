34942b2f3620526c7a3dc81120b5c7b97a45dad5
linusbao <linusbao123@gmail.com>
2024-09-26 07:38:45 +0100

    basic hom functionality is working (pre sum encoder and whatnot)

--- grit/encoder/composed_encoders.py
+++ grit/encoder/composed_encoders.py
@@ -14,6 +14,9 @@ from grit.encoder.type_dict_encoder import TypeDictNodeEncoder
 from grit.encoder.linear_node_encoder import LinearNodeEncoder
 from grit.encoder.equivstable_laplace_pos_encoder import EquivStableLapPENodeEncoder
 
+#for homcounts #ADDITION
+from grit.encoder.MLP_count_encoder import MLPNodeCountEncoder, MLPGraphCountEncoder, MLPNodeCountEncoderX2, NodeCountSum
+
 
 def concat_node_encoders(encoder_classes, pe_enc_names):
     """
@@ -46,9 +49,24 @@ def concat_node_encoders(encoder_classes, pe_enc_names):
                 self.encoder2 = self.enc2_cls(dim_emb)
             else:
                 # PE dims can only be gathered once the cfg is loaded.
-                enc2_dim_pe = getattr(cfg, f"posenc_{self.enc2_name}").dim_pe
-            
-                self.encoder1 = self.enc1_cls(dim_emb - enc2_dim_pe)
+                enc2_dim_pe = getattr(cfg, f"posenc_{self.enc2_name}").dim_pe if hasattr(cfg, f"posenc_{self.enc2_name}") else getattr(cfg, f"ctenc_{self.enc2_name}").dim_ct
+                
+                # scale the output dimension up if we use a trig_encoder (without using an MLP after to downproject back to dim_ct)
+                if hasattr(cfg, f"ctenc_{self.enc2_name}") and hasattr(getattr(cfg, f"ctenc_{self.enc2_name}"), "trig_enc") and getattr(cfg, f"ctenc_{self.enc2_name}").trig_enc.use and not getattr(cfg, f"ctenc_{self.enc2_name}").trig_enc.post_trig_fc:
+                    enc_cfg = getattr(cfg, f"ctenc_{self.enc2_name}")
+                    d = enc_cfg.trig_enc.d
+                    #find the OG count input dim (which will be scaled by d)
+                    if hasattr(enc_cfg, "dim_ogct"): #in the case we are just using a usual NodeCountEnc
+                        input_dim = enc_cfg.dim_ogct
+                    enc2_dim_pe = input_dim * d
+
+                if (not hasattr(cfg, f"ctenc_{self.enc2_name}") or not hasattr(getattr(cfg, f"ctenc_{self.enc2_name}"), 'stack_on_h')) and (not hasattr(cfg, f"posenc_{self.enc2_name}.stack_on_h") or not hasattr(getattr(cfg, f"posenc_{self.enc2_name}"), 'stack_on_h')):
+                    self.encoder1 = self.enc1_cls(dim_emb - enc2_dim_pe)
+                elif hasattr(cfg, f"ctenc_{self.enc2_name}") and hasattr(getattr(cfg, f"ctenc_{self.enc2_name}"), 'stack_on_h'):
+                    self.encoder1 = self.enc1_cls(dim_emb - enc2_dim_pe) if getattr(getattr(cfg, f"ctenc_{self.enc2_name}"), "stack_on_h") == False else self.enc1_cls(dim_emb)
+                elif hasattr(cfg, f"posenc_{self.enc2_name}") and hasattr(getattr(cfg, f"posenc_{self.enc2_name}"), 'stack_on_h'):
+                    self.encoder1 = self.enc1_cls(dim_emb - enc2_dim_pe) if getattr(getattr(cfg, f"posenc_{self.enc2_name}"), "stack_on_h") == False else self.enc1_cls(dim_emb)
+
                 self.encoder2 = self.enc2_cls(dim_emb, expand_x=False)
 
         def forward(self, batch):
@@ -68,11 +86,44 @@ def concat_node_encoders(encoder_classes, pe_enc_names):
         def __init__(self, dim_emb):
             super().__init__()
             # PE dims can only be gathered once the cfg is loaded.
-            enc2_dim_pe = getattr(cfg, f"posenc_{self.enc2_name}").dim_pe
-            enc3_dim_pe = getattr(cfg, f"posenc_{self.enc3_name}").dim_pe
-            self.encoder1 = self.enc1_cls(dim_emb - enc2_dim_pe - enc3_dim_pe)
-            self.encoder2 = self.enc2_cls(dim_emb - enc3_dim_pe, expand_x=False)
-            self.encoder3 = self.enc3_cls(dim_emb, expand_x=False)
+            enc2_dim_pe = getattr(cfg, f"posenc_{self.enc2_name}").dim_pe if hasattr(cfg, f"posenc_{self.enc2_name}") else getattr(cfg, f"ctenc_{self.enc2_name}").dim_ct
+            enc3_dim_pe = getattr(cfg, f"posenc_{self.enc3_name}").dim_pe if hasattr(cfg, f"posenc_{self.enc3_name}") else getattr(cfg, f"ctenc_{self.enc3_name}").dim_ct
+
+            # scale the output dimension up if we use a trig_encoder (without using an MLP after to downproject back to dim_ct)
+            if hasattr(cfg, f"ctenc_{self.enc2_name}") and hasattr(getattr(cfg, f"ctenc_{self.enc2_name}"), "trig_enc") and getattr(cfg, f"ctenc_{self.enc2_name}").trig_enc.use and not getattr(cfg, f"ctenc_{self.enc2_name}").trig_enc.post_trig_fc:
+                enc_cfg = getattr(cfg, f"ctenc_{self.enc2_name}")
+                d = enc_cfg.trig_enc.d
+                #find the OG count input dim (which will be scaled by d)
+                if hasattr(enc_cfg, "dim_ogct"): #in the case we are just using a usual NodeCountEnc
+                    input_dim = enc_cfg.dim_ogct
+                enc2_dim_pe = input_dim * d
+            if hasattr(cfg, f"ctenc_{self.enc3_name}") and hasattr(getattr(cfg, f"ctenc_{self.enc3_name}"), "trig_enc") and getattr(cfg, f"ctenc_{self.enc3_name}").trig_enc.use and not getattr(cfg, f"ctenc_{self.enc3_name}").trig_enc.post_trig_fc:
+                enc_cfg = getattr(cfg, f"ctenc_{self.enc3_name}")
+                d = enc_cfg.trig_enc.d
+                #find the OG count input dim (which will be scaled by d)
+                if hasattr(enc_cfg, "dim_ogct"): #in the case we are just using a usual NodeCountEnc
+                    input_dim = enc_cfg.dim_ogct
+                enc3_dim_pe = input_dim * d
+
+            if (hasattr(cfg, f"ctenc_{self.enc2_name}") and hasattr(getattr(cfg, f"ctenc_{self.enc2_name}"),"stack_on_h" ) and getattr(cfg, f"ctenc_{self.enc2_name}").stack_on_h == True) and (hasattr(cfg, f"posenc_{self.enc3_name}") and hasattr(getattr(cfg, f"posenc_{self.enc3_name}"), 'stack_on_h') and getattr(cfg, f"posenc_{self.enc3_name}").stack_on_h == True):
+                dim1 = dim_emb
+                dim2 = dim_emb
+                dim3 = dim_emb
+            elif (hasattr(cfg, f"ctenc_{self.enc3_name}") and hasattr(getattr(cfg, f"ctenc_{self.enc3_name}"),"stack_on_h" ) and getattr(cfg, f"ctenc_{self.enc3_name}.stack_on_h") == True) and (hasattr(cfg, f"posenc_{self.enc2_name}.stack_on_h") and hasattr(getattr(cfg, f"posenc_{self.enc2_name}"),"stack_on_h" ) and getattr(cfg, f"posenc_{self.enc2_name}").stack_on_h == True):
+                dim1 = dim_emb
+                dim2 = dim_emb
+                dim3 = dim_emb
+            else:
+                dim1 = dim_emb - enc2_dim_pe - enc3_dim_pe
+                dim2 = dim_emb - enc3_dim_pe
+                dim3 = dim_emb
+
+            self.encoder1 = self.enc1_cls(dim1)
+            self.encoder2 = self.enc2_cls(dim2, expand_x=False)
+            self.encoder3 = self.enc3_cls(dim3, expand_x=False)
+            # self.encoder1 = self.enc1_cls(dim_emb - enc2_dim_pe - enc3_dim_pe)
+            # self.encoder2 = self.enc2_cls(dim_emb - enc3_dim_pe, expand_x=False)
+            # self.encoder3 = self.enc3_cls(dim_emb, expand_x=False)
 
         def forward(self, batch):
             batch = self.encoder1(batch)
@@ -115,6 +166,11 @@ pe_encs = {'LapPE': LapPENodeEncoder,
            'EquivStableLapPE': EquivStableLapPENodeEncoder
            }
 
+# Count Encoding node encoders. #ADDITION
+ct_encs = {'NodeCountEnc': MLPNodeCountEncoder,
+           'GraphCountEnc': MLPGraphCountEncoder,
+           'NodeCountEncX2': MLPNodeCountEncoderX2}
+
 # Concat dataset-specific and PE encoders.
 for ds_enc_name, ds_enc_cls in ds_encs.items():
     for pe_enc_name, pe_enc_cls in pe_encs.items():
@@ -139,3 +195,12 @@ for ds_enc_name, ds_enc_cls in ds_encs.items():
         concat_node_encoders([ds_enc_cls, SignNetNodeEncoder, RWSENodeEncoder],
                              ['SignNet', 'RWSE'])
     )
+
+# Concat dataset-specific and count encoders. #ADDITION
+for ds_enc_name, ds_enc_cls in ds_encs.items():
+    for ct_enc_name, ct_enc_cls in ct_encs.items():
+        register_node_encoder(
+            f"{ds_enc_name}+{ct_enc_name}",
+            concat_node_encoders([ds_enc_cls, ct_enc_cls],
+                                 [ct_enc_name])
+        )
